db5b3a395c45e0a6ee08d2df627b997f4b4b9e0c
Mohammed <mohammedmohsin68217@gmail.com>
2018-11-15 02:47:29 +0200

    Initial Commit

---
+++ LocalizationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocalizationApp
+//
+//  Created by Mohammed Mohsin Sayed on 11/15/18.
+//  Copyright © 2018 Mohammed Mohsin Sayed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
